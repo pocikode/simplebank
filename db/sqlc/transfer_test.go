4cf9b113eb8416d570db28d6f5743ee0120b250e
Agus Supriyatna <aguzsupriyatna7@gmail.com>
2023-11-24 14:17:44 +0700

    🚧 chore(entry_test.go): add tests for creating, getting, and listing entries to ensure proper functionality 🚧 chore(transfer_test.go): add tests for creating, getting, and listing transfers to ensure proper functionality

---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,81 @@
+package db
+
+import (
+	"context"
+	"github.com/go-faker/faker/v4"
+	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/require"
+	"strings"
+	"testing"
+	"time"
+)
+
+func createRandomTransfer(t *testing.T, account1, account2 Account) Transfer {
+	amountWithCurrency := strings.Split(faker.AmountWithCurrency(), " ")
+	amountDecimal, _ := decimal.NewFromString(amountWithCurrency[1])
+
+	arg := CreateTransferParams{
+		FromAccountID: account1.ID,
+		ToAccountID:   account2.ID,
+		Amount:        amountWithCurrency[1],
+	}
+
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, arg.FromAccountID, transfer.FromAccountID)
+	require.Equal(t, arg.ToAccountID, transfer.ToAccountID)
+	require.True(t, transfer.Amount.Equal(amountDecimal))
+
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+
+	return transfer
+}
+
+func TestCreateTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	createRandomTransfer(t, account1, account2)
+}
+
+func TestGetTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	transfer1 := createRandomTransfer(t, account1, account2)
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer2)
+
+	require.Equal(t, transfer1.ID, transfer2.ID)
+	require.Equal(t, transfer1.FromAccountID, transfer2.FromAccountID)
+	require.Equal(t, transfer1.ToAccountID, transfer2.ToAccountID)
+	require.Equal(t, transfer1.Amount, transfer2.Amount)
+	require.WithinDuration(t, transfer1.CreatedAt, transfer2.CreatedAt, time.Second)
+}
+
+func TestListTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	for i := 0; i < 10; i++ {
+		createRandomTransfer(t, account1, account2)
+	}
+
+	arg := ListTransfersParams{
+		FromAccountID: account1.ID,
+		ToAccountID:   account2.ID,
+		Limit:         5,
+		Offset:        5,
+	}
+
+	transfers, err := testQueries.ListTransfers(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, transfers, 5)
+
+	for _, transfer := range transfers {
+		require.NotEmpty(t, transfer)
+		require.Equal(t, transfer.FromAccountID, arg.FromAccountID)
+		require.Equal(t, transfer.ToAccountID, arg.ToAccountID)
+	}
+}
